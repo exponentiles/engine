19562eb60d5da9c8d098d353943044e8fa3598dc
jstoone <jstoone@drk.sh>
2021-03-29 01:17:20 +0200

    Use new grid import/export to simplify tests.

--- tests/EngineOperationsTest.php
+++ tests/EngineOperationsTest.php
@@ -8,95 +8,56 @@ use PHPUnit\Framework\TestCase;
 
 class EngineOperationsTest extends TestCase
 {
-    public function test_it_can_steer_south()
+    public function test_it_can_steer_south_array()
     {
         $engine = new Engine();
 
-        /*
-         * +---+---+---+---+
-         * | 2 | 2 | 2 | 2 |
-         * +---+---+---+---+
-         * | 2 | 2 | 4 | 2 |
-         * +---+---+---+---+
-         * | 4 | 2 | 2 | 4 |
-         * +---+---+---+---+
-         * | 0 | 2 | 2 | 4 |
-         * +---+---+---+---+
-         */
-        $engine->grid->tiles = [
-            [new Tile(0, 0, 2), new Tile(0, 1, 2), new Tile(0, 2, 4), new Tile(0, 3, 0)],
-            [new Tile(1, 0, 2), new Tile(1, 1, 2), new Tile(1, 2, 2), new Tile(1, 3, 2)],
-            [new Tile(2, 0, 2), new Tile(2, 1, 4), new Tile(2, 2, 2), new Tile(2, 3, 2)],
-            [new Tile(3, 0, 2), new Tile(3, 1, 2), new Tile(3, 2, 4), new Tile(3, 3, 4)],
-        ];
+        $engine->grid->fromArray(
+            [
+                2, 2, 2, 2,
+                2, 2, 4, 2,
+                4, 2, 2, 4,
+                0, 2, 2, 4,
+            ]
+        );
 
         $engine->steer('SOUTH');
 
-        /*
-         * +---+---+---+---+
-         * | 0 | 0 | 0 | 0 |
-         * +---+---+---+---+
-         * | 0 | 0 | 2 | 0 |
-         * +---+---+---+---+
-         * | 4 | 4 | 4 | 4 |
-         * +---+---+---+---+
-         * | 4 | 4 | 4 | 8 |
-         * +---+---+---+---+
-         */
         $this->assertEquals(
             [
-                [new Tile(0, 0, 0), new Tile(0, 1, 0), new Tile(0, 2, 4), new Tile(0, 3, 4)],
-                [new Tile(1, 0, 0), new Tile(1, 1, 0), new Tile(1, 2, 4), new Tile(1, 3, 4)],
-                [new Tile(2, 0, 0), new Tile(2, 1, 2), new Tile(2, 2, 4), new Tile(2, 3, 4)],
-                [new Tile(3, 0, 0), new Tile(3, 1, 0), new Tile(3, 2, 4), new Tile(3, 3, 8)],
+                0, 0, 0, 0,
+                0, 0, 2, 0,
+                4, 4, 4, 4,
+                4, 4, 4, 8,
             ],
-            $engine->grid->tiles
+            $engine->grid->toArray()
         );
     }
+
 
     public function test_it_can_steer_north()
     {
         $engine = new Engine();
 
-        /*
-         * +---+---+---+---+
-         * | 2 | 2 | 2 | 2 |
-         * +---+---+---+---+
-         * | 2 | 2 | 4 | 2 |
-         * +---+---+---+---+
-         * | 4 | 2 | 2 | 4 |
-         * +---+---+---+---+
-         * | 0 | 2 | 2 | 4 |
-         * +---+---+---+---+
-         */
-        $engine->grid->tiles = [
-            [new Tile(0, 0, 2), new Tile(0, 1, 2), new Tile(0, 2, 4), new Tile(0, 3, 0)],
-            [new Tile(1, 0, 2), new Tile(1, 1, 2), new Tile(1, 2, 2), new Tile(1, 3, 2)],
-            [new Tile(2, 0, 2), new Tile(2, 1, 4), new Tile(2, 2, 2), new Tile(2, 3, 2)],
-            [new Tile(3, 0, 2), new Tile(3, 1, 2), new Tile(3, 2, 4), new Tile(3, 3, 4)],
-        ];
+        $engine->grid->fromArray(
+            [
+                0, 0, 0, 0,
+                0, 0, 2, 0,
+                4, 4, 4, 4,
+                4, 4, 4, 8,
+            ]
+        );
 
         $engine->steer('NORTH');
 
-        /*
-         * +---+---+---+---+
-         * | 4 | 4 | 2 | 4 |
-         * +---+---+---+---+
-         * | 4 | 4 | 4 | 8 |
-         * +---+---+---+---+
-         * | 0 | 0 | 4 | 0 |
-         * +---+---+---+---+
-         * | 0 | 0 | 0 | 0 |
-         * +---+---+---+---+
-         */
         $this->assertEquals(
             [
-                [new Tile(0, 0, 4), new Tile(0, 1, 4), new Tile(0, 2, 0), new Tile(0, 3, 0)],
-                [new Tile(1, 0, 4), new Tile(1, 1, 4), new Tile(1, 2, 0), new Tile(1, 3, 0)],
-                [new Tile(2, 0, 2), new Tile(2, 1, 4), new Tile(2, 2, 4), new Tile(2, 3, 0)],
-                [new Tile(3, 0, 4), new Tile(3, 1, 8), new Tile(3, 2, 0), new Tile(3, 3, 0)],
+                8, 8, 2, 4,
+                0, 0, 8, 8,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
             ],
-            $engine->grid->tiles
+            $engine->grid->toArray()
         );
     }
 
@@ -104,45 +65,25 @@ class EngineOperationsTest extends TestCase
     {
         $engine = new Engine();
 
-        /*
-         * +---+---+---+---+
-         * | 2 | 2 | 2 | 2 |
-         * +---+---+---+---+
-         * | 2 | 2 | 4 | 2 |
-         * +---+---+---+---+
-         * | 4 | 2 | 2 | 4 |
-         * +---+---+---+---+
-         * | 0 | 2 | 2 | 4 |
-         * +---+---+---+---+
-         */
-        $engine->grid->tiles = [
-            [new Tile(0, 0, 2), new Tile(0, 1, 2), new Tile(0, 2, 4), new Tile(0, 3, 0)],
-            [new Tile(1, 0, 2), new Tile(1, 1, 2), new Tile(1, 2, 2), new Tile(1, 3, 2)],
-            [new Tile(2, 0, 2), new Tile(2, 1, 4), new Tile(2, 2, 2), new Tile(2, 3, 2)],
-            [new Tile(3, 0, 2), new Tile(3, 1, 2), new Tile(3, 2, 4), new Tile(3, 3, 4)],
-        ];
+        $engine->grid->fromArray(
+            [
+                8, 8, 2, 4,
+                0, 0, 8, 8,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+            ],
+        );
 
         $engine->steer('EAST');
 
-        /*
-         * +---+---+---+---+
-         * | 0 | 0 | 4 | 4 |
-         * +---+---+---+---+
-         * | 0 | 4 | 4 | 2 |
-         * +---+---+---+---+
-         * | 0 | 4 | 4 | 4 |
-         * +---+---+---+---+
-         * | 0 | 0 | 4 | 4 |
-         * +---+---+---+---+
-         */
         $this->assertEquals(
             [
-                [new Tile(0, 0, 0), new Tile(0, 1, 0), new Tile(0, 2, 0), new Tile(0, 3, 0)],
-                [new Tile(1, 0, 0), new Tile(1, 1, 4), new Tile(1, 2, 4), new Tile(1, 3, 0)],
-                [new Tile(2, 0, 4), new Tile(2, 1, 4), new Tile(2, 2, 4), new Tile(2, 3, 4)],
-                [new Tile(3, 0, 4), new Tile(3, 1, 2), new Tile(3, 2, 4), new Tile(3, 3, 4)],
+                0, 16, 2, 4,
+                0, 0, 0, 16,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
             ],
-            $engine->grid->tiles
+            $engine->grid->toArray()
         );
     }
 
@@ -150,45 +91,25 @@ class EngineOperationsTest extends TestCase
     {
         $engine = new Engine();
 
-        /*
-         * +---+---+---+---+
-         * | 2 | 2 | 2 | 2 |
-         * +---+---+---+---+
-         * | 2 | 2 | 4 | 2 |
-         * +---+---+---+---+
-         * | 4 | 2 | 2 | 4 |
-         * +---+---+---+---+
-         * | 0 | 2 | 2 | 4 |
-         * +---+---+---+---+
-         */
-        $engine->grid->tiles = [
-            [new Tile(0, 0, 2), new Tile(0, 1, 2), new Tile(0, 2, 4), new Tile(0, 3, 0)],
-            [new Tile(1, 0, 2), new Tile(1, 1, 2), new Tile(1, 2, 2), new Tile(1, 3, 2)],
-            [new Tile(2, 0, 2), new Tile(2, 1, 4), new Tile(2, 2, 2), new Tile(2, 3, 2)],
-            [new Tile(3, 0, 2), new Tile(3, 1, 2), new Tile(3, 2, 4), new Tile(3, 3, 4)],
-        ];
+        $engine->grid->fromArray(
+            [
+                0, 16, 2, 4,
+                0, 0, 0, 16,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
+            ],
+        );
 
         $engine->steer('WEST');
 
-        /*
-         * +---+---+---+---+
-         * | 4 | 4 | 0 | 0 |
-         * +---+---+---+---+
-         * | 4 | 4 | 2 | 0 |
-         * +---+---+---+---+
-         * | 4 | 4 | 4 | 0 |
-         * +---+---+---+---+
-         * | 4 | 4 | 0 | 0 |
-         * +---+---+---+---+
-         */
         $this->assertEquals(
             [
-                [new Tile(0, 0, 4), new Tile(0, 1, 4), new Tile(0, 2, 4), new Tile(0, 3, 4)],
-                [new Tile(1, 0, 4), new Tile(1, 1, 4), new Tile(1, 2, 4), new Tile(1, 3, 4)],
-                [new Tile(2, 0, 0), new Tile(2, 1, 2), new Tile(2, 2, 4), new Tile(2, 3, 0)],
-                [new Tile(3, 0, 0), new Tile(3, 1, 0), new Tile(3, 2, 0), new Tile(3, 3, 0)],
+                16, 2, 4, 0,
+                16, 0, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 0,
             ],
-            $engine->grid->tiles
+            $engine->grid->toArray()
         );
     }
 }
