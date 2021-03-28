78faa79182cb4049ec7da44361e40fc7a00161a8
jstoone <jstoone@drk.sh>
2021-03-28 21:45:48 +0200

    Extract grid manipulation to own class.

---
+++ src/Operator.php
@@ -0,0 +1,77 @@
+<?php
+
+
+namespace Exponentiles\Engine;
+
+
+use Illuminate\Support\Arr;
+
+class Operator
+{
+    /**
+     * @param array<int, int> $values
+     * @return array<int, int>
+     */
+    public static function slide(array $values): array
+    {
+        $length = count($values);
+
+        $values = array_filter($values);
+
+        $values = array_pad($values, -$length, 0);
+
+        return $values;
+    }
+
+    /**
+     * @param array<int, int> $values
+     * @return array<int, int>
+     */
+    public static function combine(array $values): array
+    {
+        // Loop backwards through tile values.
+        for ($i = count($values) - 1; $i >= 1; $i--) {
+            $current = $values[$i];
+            $previous = $values[$i - 1] ?? null;
+
+            if ($current !== $previous) {
+                continue;
+            }
+
+            // Combine onto current value.
+            $values[$i] = $current + $previous;
+
+            // Empty previous value.
+            $values[$i - 1] = 0;
+        }
+
+        return $values;
+    }
+
+    /**
+     * @param array<int, int> $values
+     * @return array<int, int>
+     */
+    public static function move(array $values): array
+    {
+        $values = static::slide($values);
+        $values = static::combine($values);
+        $values = static::slide($values);
+
+        return $values;
+    }
+
+    public static function rotate(array $grid): array
+    {
+        $gridSize = count($grid);
+        $gridCopy = $grid;
+
+        for ($i = 0; $i < $gridSize; $i++) {
+            for ($j = 0; $j < $gridSize; $j++) {
+                $gridCopy[$i][$j] = $grid[$j][$i];
+            }
+        }
+
+        return $gridCopy;
+    }
+}
---
+++ tests/OperatorTest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Exponentiles\Engine\Tests;
+
+use Exponentiles\Engine\Engine;
+use Exponentiles\Engine\Grid;
+use Exponentiles\Engine\Operator;
+use Exponentiles\Engine\Tile;
+use Illuminate\Support\Arr;
+use PHPUnit\Framework\TestCase;
+
+class OperatorTest extends TestCase
+{
+    public function test_it_can_slide()
+    {
+        $this->assertSame(
+            [0, 0, 2, 4],
+            Operator::slide([2, 0, 4, 0])
+        );
+    }
+
+    public function test_it_can_combine()
+    {
+        $this->assertSame(
+            [0, 4, 4, 0],
+            Operator::combine([2, 2, 4, 0])
+        );
+    }
+
+    public function test_it_can_move()
+    {
+        $this->assertSame(
+            [0, 0, 4, 4],
+            Operator::move([0, 2, 2, 4])
+        );
+
+        $this->assertSame(
+            [0, 0, 4, 4],
+            Operator::move([2, 2, 2, 2])
+        );
+
+        $this->assertSame(
+            [0, 2, 4, 4],
+            Operator::move([2, 4, 2, 2])
+        );
+    }
+
+    public function test_it_can_rotate_grid()
+    {
+        $grid = [
+            ['a', 'b', 'c'],
+            ['d', 'e', 'f'],
+            ['g', 'h', 'i'],
+        ];
+
+        $this->assertSame(
+            [
+                ['a', 'd', 'g'],
+                ['b', 'e', 'h'],
+                ['c', 'f', 'i'],
+            ],
+            Operator::rotate($grid)
+        );
+    }
+}
