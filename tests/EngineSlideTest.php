a27ea9e295728f3bafa52af575aa87483eb0fa53
jstoone <jstoone@drk.sh>
2021-03-28 15:21:07 +0200

    Update test case to cover more ground.

--- tests/EngineSlideTest.php
+++ tests/EngineSlideTest.php
@@ -19,12 +19,14 @@ class EngineSlideTest extends TestCase
          * +---+---+---+
          * | 2 | 0 | 0 |
          * +---+---+---+
-         * | 0 | 0 | 0 |
+         * | 4 | 2 | 0 |
          * +---+---+---+
          * | 0 | 0 | 2 |
          * +---+---+---+
          */
         $grid->getTile(0, 0)->value = 2;
+        $grid->getTile(0, 1)->value = 4;
+        $grid->getTile(1, 1)->value = 2;
         $grid->getTile(2, 2)->value = 2;
 
         $engine->slide($grid, 'SOUTH');
@@ -33,18 +35,19 @@ class EngineSlideTest extends TestCase
          * +---+---+---+
          * | 0 | 0 | 0 |
          * +---+---+---+
-         * | 0 | 0 | 0 |
+         * | 2 | 0 | 0 |
          * +---+---+---+
-         * | 2 | 0 | 2 |
+         * | 4 | 2 | 2 |
          * +---+---+---+
          */
         $this->assertEquals(
             [
-                [new Tile(0, 0, 0), new Tile(0, 1, 0), new Tile(0, 2, 2)],
-                [new Tile(1, 0, 0), new Tile(1, 1, 0), new Tile(1, 2, 0)],
+                [new Tile(0, 0, 0), new Tile(0, 1, 2), new Tile(0, 2, 4)],
+                [new Tile(1, 0, 0), new Tile(1, 1, 0), new Tile(1, 2, 2)],
                 [new Tile(2, 0, 0), new Tile(2, 1, 0), new Tile(2, 2, 2)],
             ],
             $grid->tiles
         );
     }
+
 }
